6772477e8eb061d35aeaaf4cfd8e3b2ffcf6602f
Aman Agarwal <amanag0101@gmail.com>
2023-08-27 16:07:47 +0530

    style changes and made the page responsive

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, MenuItem, Select } from "@mui/material";
+import { FormControl, MenuItem, Select } from "@mui/material";
 import React, { useState } from "react";
 import styles from "./event.module.css";
 
@@ -37,28 +37,29 @@ export const Event = () => {
 
   return (
     <div className={styles.events}>
-      <div className="mt-16"></div>
       <h1 className={styles.heading}>Events at MFC VIT</h1>
 
       <div className={styles.about}>
         <p>
           MFC VIT Boasts A Wide Array Of Successful Events Conducted Throughout
+          The Year Including Speaker Sessions With Renowned Technocrats, The
+          Year Including Speaker Sessions With Renowned Technocrats, Competitive
+          Coding Events, And Many More.
         </p>
-        <p>The Year Including Speaker Sessions With Renowned Technocrats,</p>
-        <p>Competitive Coding Events, And Many More.</p>
       </div>
 
       <div className={styles.year_select}>
         <FormControl sx={{ width: "250px" }}>
           <Select
-            className="bg-[#ff6a00] font-montserrat"
+            className="bg-[#ff6a00]"
             value={year}
             onChange={handleChange}
             sx={{
-              my: 4,
+              my: 2,
               borderRadius: "50px",
               fontFamily: '"Montserrat", sans-serif',
               fontSize: "1.2em",
+              height: "2.5em",
             }}
           >
             <MenuItem value={2018}>2018</MenuItem>
@@ -72,11 +73,12 @@ export const Event = () => {
       </div>
 
       <div className={styles.carousel}>
-        {/* <p className={styles.slide_count}>{slideIndex + 1} / {slides.length}</p> */}
         <div className={styles.content}>
           <img className={styles.col_1} src={slides[slideIndex + 0]} />
-          <div class="box-border w-[0.04rem] h-[16rem] border-r-[0.7px] border-solid border-saddlebrown mx-2"></div>
-          {/* <div class="box-border h-full w-[1px] border-r-[1px] border-solid border-black mx-[1px]"></div> */}
+
+          {/* <div class="box-border w-[0.04rem] h-[16rem] border-r-[0.7px] border-solid border-saddlebrown mx-2"></div> */}
+          <div className={styles.box_border}></div>
+
           <div className={styles.col_2}>
             <h1 className="text-black">Event Name</h1>
             <p className="text-black">
@@ -87,8 +89,8 @@ export const Event = () => {
             </p>
           </div>
         </div>
-        {/* <Button className={styles.register}>Register for Event</Button> */}
       </div>
+
       <div className="flex w-full justify-center h-8">
         <div className="flex justify-between items-center w-1/4">
           <img src="arrow-left.svg" onClick={prevSlide} />
